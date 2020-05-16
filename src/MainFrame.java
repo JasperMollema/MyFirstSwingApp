43c5a6a6762bc38affc4f9aef81e621bf08da424
Jasper Mollema <jasper.mollema@gmail.com>
2020-05-16 21:16:05 +0200

    Add MainFrame

---
+++ src/MainFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        super("Hello World");
+        
+        setSize(500, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
